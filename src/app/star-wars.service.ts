7b826df2d9c03698efcebe22cd75651bd6ff9aad
johannasjo <64302410+johannasjo@users.noreply.github.com>
2023-01-17 21:13:15 +0100

    add observable for planets

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,17 +9,21 @@ export class StarwarsService {
   baseUrl = `https://swapi.dev/api`;
   constructor(private http: HttpClient) {
     this.fetchCharacters();
+    this.fetchPlanets();
   }
 
   // never expose behaviourSubject outside of service, as it gives access to
   // emit data through the service
   private _characters$: BehaviorSubject<any> = new BehaviorSubject([]);
+  private _planets$: BehaviorSubject<any> = new BehaviorSubject([]);
+
   public characters$: Observable<any> = this._characters$.asObservable();
   public charactersWithVehicles$: Observable<any> = this.characters$.pipe(
     map((characters) => {
       return characters.filter((character) => character.vehicles.length);
     })
   );
+  public planets$: Observable<any> = this._planets$.asObservable();
 
   fetchCharacters() {
     this.http
@@ -28,6 +32,17 @@ export class StarwarsService {
       .subscribe((characters) => {
         this._characters$.next(characters);
       });
+  }
+
+  fetchPlanets() {
+    this.http
+      .get(`${this.baseUrl}/planets`)
+      .pipe(
+        tap(console.log),
+        map((response) => response.results),
+        tap((planets) => console.log('planets', planets))
+      )
+      .subscribe();
   }
 
   // not really neccessary if I have public observables

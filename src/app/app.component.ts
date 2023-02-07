ce189744a9a5bf51fb98da7c6bdc734be133e9b1
johannasjo <64302410+johannasjo@users.noreply.github.com>
2023-02-07 08:51:38 +0100

    start on udemy course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,29 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   constructor(private starWarsService: StarwarsService) {}
+
+  // basic observable, subscription and observer
+
+  public newObservable$ = new Observable<string>((subscriber) => {
+    console.log('Observable executed');
+    subscriber.next('Alice');
+    setTimeout(() => {
+      subscriber.next('Charlie');
+      setTimeout(() => {
+        subscriber.error(new Error('Failure'));
+      }, 3000);
+    }, 1000);
+    return () => {
+      console.log('tear down');
+    };
+  }).subscribe({
+    next: (value) => console.log(value),
+    complete: () => console.log('completed'),
+    error: (err) => console.log('error message is: ', err.message),
+  });
+
+  // star wars observables
+
   public characters$: Observable<any> = this.starWarsService.characters$;
   public charactersWithVehicles$: Observable<any> =
     this.starWarsService.charactersWithVehicles$;
